770ecd534092ffb9b0bd9b597f3cb37f7a0abfea
Knyc8 <kchen9712@bths.edu>
2022-11-27 17:53:51 -0500

    Implemented methods for the players stats, bear stats, and attack

--- src/HuntingGame.java
+++ src/HuntingGame.java
@@ -1,4 +1,11 @@
 public class HuntingGame {
+    //private variables
+    private int playerHp = 10;
+    private int bearHp = 25;
+    private int cryptidHp = 50;
+    private int playerDmg = 0;
+    private int beastDmg = 0;
+
     //default constructor
 
     /***
@@ -34,6 +41,52 @@ public class HuntingGame {
         return skyGrid.substring(0, 2 * guessShot - 1) + "O" + skyGrid.substring(2 * guessShot);
     }
 
-    //bear-related code:
+    public void attack(){
+        int damage = (int)(Math.random() * 6) + 1;
+        if (damage <= 3)
+        {
+            playerDmg = 1;
+        }
+        else if (damage <= 5)
+        {
+            playerDmg = 3;
+        }
+        else {
+            playerDmg = 10;
+        }
+    }
 
+    public void updatePlayerStats() {
+        playerHp -= beastDmg;
+    }
+
+    //bear-related code:
+    public String bearFight()
+    {
+        if (bearHp <= 0) {
+            bearHp = 0;
+            return "Bear health: " + bearHp +
+                    "\nYour health: " + playerHp +
+                    "\nThe bear collapses onto the floor." +
+                    "\nA bear is added to your inventory.";
+        }
+        else if (playerHp <= 0) {
+            playerHp = 0;
+            return "Bear health: " + bearHp +
+                    "\nYour health: " + playerHp +
+                    "\nYou collapse onto the floor." +
+                    "\nThe bear runs away.";
+        }
+        else {
+            return "Bear health: " + bearHp +
+                    "\nYour health: " + playerHp +
+                    "\nThe bear dealt " + beastDmg + " damage!" +
+                    "\nYou dealt " + playerDmg + "damage!";
+        }
+    }
+
+    public void updateBearStats()
+    {
+        bearHp -= playerDmg;
+    }
 }
